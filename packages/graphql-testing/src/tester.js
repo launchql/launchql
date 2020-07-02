f6daab5a2d42e8483286eaa8d442344779fa9edd
Dan Lynch <pyramation@gmail.com>
2020-07-02 02:36:36 -0700

    update

--- packages/graphql-testing/src/tester.js
+++ packages/graphql-testing/src/tester.js
@@ -240,7 +240,7 @@ export const GraphQLTest = ({ settings }) => {
           // Rollback the transaction so no changes are written to the DB - this
           // makes our tests fairly deterministic.
 
-          await replacementPgClient.query('commit');
+          await replacementPgClient.query('rollback');
           replacementPgClient.release();
         }
         return getResult;
