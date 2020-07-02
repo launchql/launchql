7d0eebad62a191d50e49ec7a594ae2630045c0a3
Dan Lynch <pyramation@gmail.com>
2020-07-02 15:05:12 -0700

    update

--- packages/db-testing/src/cmd/droptemplatedb.js
+++ packages/db-testing/src/cmd/droptemplatedb.js
@@ -2,18 +2,16 @@
 
 import { dropdb } from '../lib/db';
 import { getOpts } from '../lib/testing';
+import env from '../env';
 
-if (!process.env.PGTEMPLATE_DATABASE) {
-  require('dotenv').load();
-  if (!process.env.PGTEMPLATE_DATABASE) {
-    throw new Error('no PGTEMPLATE_DATABASE defined in env!');
-  }
+if (!env.PGTEMPLATE_DATABASE) {
+  throw new Error('no PGTEMPLATE_DATABASE defined in env!');
 }
 
 const run = async () => {
   const config = await getOpts({});
   try {
-    await dropdb({ ...config, database: process.env.PGTEMPLATE_DATABASE });
+    await dropdb({ ...config, database: env.PGTEMPLATE_DATABASE });
   } catch (e) {
     console.error(e);
   }
--- packages/db-testing/src/cmd/templatedb.js
+++ packages/db-testing/src/cmd/templatedb.js
@@ -3,21 +3,19 @@
 import { TestDatabase } from '../lib/test-db';
 import { dropdb } from '../lib/db';
 import { getOpts } from '../lib/testing';
+import env from '../env';
 
-if (!process.env.PGTEMPLATE_DATABASE) {
-  require('dotenv').load();
-  if (!process.env.PGTEMPLATE_DATABASE) {
-    throw new Error('no PGTEMPLATE_DATABASE defined in env!');
-  }
+if (!env.PGTEMPLATE_DATABASE) {
+  throw new Error('no PGTEMPLATE_DATABASE defined in env!');
 }
 
-let extensions = process.env.PGEXTENSIONS;
+let extensions = env.PGEXTENSIONS;
 extensions = extensions.split(',');
 
 const run = async () => {
   const config = await getOpts({});
   try {
-    await dropdb({ ...config, database: process.env.PGTEMPLATE_DATABASE });
+    await dropdb({ ...config, database: env.PGTEMPLATE_DATABASE });
   } catch (e) {}
   const test = new TestDatabase();
   await test.init(extensions);
