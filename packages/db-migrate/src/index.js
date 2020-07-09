33abddd0be59b1ad9d677d935d746c9b24518fdd
Dan Lynch <pyramation@gmail.com>
2020-07-08 21:42:26 -0700

    fix: default

--- packages/db-migrate/src/index.js
+++ packages/db-migrate/src/index.js
@@ -224,7 +224,7 @@ ${rows
     return [new RegExp(f, 'g'), r];
   });
 
-  const replacer = (str, n = 0) => {
+  const replacer = (str = '', n = 0) => {
     if (replace[n] && replace[n].length == 2) {
       return replacer(str.replace(replace[n][0], replace[n][1]), n + 1);
     }
