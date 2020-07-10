06476b734914b4c15c9bfdce0245982cb53a4f15
Dan Lynch <pyramation@gmail.com>
2020-07-09 20:41:46 -0700

    tests

--- packages/db-utils/test/publish.test.js
+++ packages/db-utils/test/publish.test.js
@@ -3,6 +3,7 @@ import { publish } from '../src/lib/publish';
 import * as shell from 'shelljs';
 const TMPDIR = process.env.TMPDIR;
 import { random } from '../src/lib/random';
+import { resolve as pr } from 'path';
 import { sync as mkdirp } from 'mkdirp';
 import { sync as glob } from 'glob';
 import { sync as rimraf } from 'rimraf';
@@ -11,20 +12,23 @@ import { writeFileSync } from 'fs';
 describe('publish', () => {
   let dir, projDir;
   beforeEach(async () => {
-    dir = TMPDIR + '/' + random();
+    dir = pr(TMPDIR + '/' + random());
     shell.cp('-r', __dirname + '/../__fixtures__/publish', dir);
     process.env.SKITCH_PATH = dir;
     process.env.SQITCH_PATH = dir + '/packages/secrets';
     process.chdir(dir);
   });
   it('publish an extension', async () => {
-    const filesBefore = glob('**');
+    const filesBefore = glob(dir + '/**')
+      .map((f) => f.replace(dir, '').replace(/^\//, ''))
+      .filter((i) => i);
     expect(filesBefore).toMatchSnapshot();
 
     await publish('secrets', 'minor');
 
-    const filesAfter = glob('**');
+    const filesAfter = glob(dir + '/**')
+      .map((f) => f.replace(dir, '').replace(/^\//, ''))
+      .filter((i) => i);
     expect(filesAfter).toMatchSnapshot();
-
   });
 });
