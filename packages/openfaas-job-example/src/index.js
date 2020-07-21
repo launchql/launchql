49a428a262926f530186214ef075f381fa11f748
Dan Lynch <pyramation@gmail.com>
2020-07-21 05:33:59 -0700

    fix: cleanup

--- packages/openfaas-job-example/src/index.js
+++ packages/openfaas-job-example/src/index.js
@@ -2,7 +2,11 @@ import app from '@launchql/openfaas-job-fn';
 import env from './env';
 
 app.post('*', async (req, res) => {
-  res.status(200).send({ hello: 'openfaas' });
+  res.status(200).send({
+    fn: 'example-fn',
+    message: 'hi I did a lot of work',
+    body: req.body
+  });
 });
 
 app.listen(env.PORT);
--- packages/openfaas-job-req/src/env.js
+++
@@ -1,6 +0,0 @@
-import { env, url } from '@launchql/openfaas-env';
-
-export default env({
-  INTERNAL_GATEWAY_URL: url(),
-  INTERNAL_JOB_REQ_URL: url()
-});
