b8f02c860c240b0e7833a64540affca1e0d42a98
Dan Lynch <pyramation@gmail.com>
2020-08-08 02:16:48 -0700

    minio integration

--- packages/stream-to-s3/src/index.js
+++ packages/stream-to-s3/src/index.js
@@ -1,35 +1,26 @@
-import S3 from 'aws-sdk/clients/s3';
+import stream from 'stream';
+import getS3 from './s3';
 import env from './env';
 import {
   streamContentType,
   ContentStream
 } from '@pyramation/content-type-stream';
 
-import stream from 'stream';
-
-const s3 = new S3({
-  region: env.AWS_REGION,
-  apiVersion: '2006-03-01'
-});
-
-// ths could go in resolver if needed...
-s3.config.update({
-  credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_KEY
-  }
-});
+const s3 = getS3();
 
 function uploadFromStream({ key, contentType, bucket }) {
-  var pass = new stream.PassThrough();
+  const pass = new stream.PassThrough();
 
-  var params = {
+  const params = {
     Body: pass,
     Key: key,
     ContentType: contentType,
     Bucket: bucket
   };
   s3.upload(params, function (err, data) {
+    if (err) {
+      return pass.emit('error', err);
+    }
     pass.emit('upload', data);
   });
 
@@ -49,7 +40,6 @@ const asyncUpload = ({ key, contentType, readStream, magic, bucket }) => {
     // content stream
     let contents;
     const contentStream = new ContentStream();
-
     const tryResolve = () => {
       if (contents && upload) {
         resolve({
@@ -87,7 +77,6 @@ export default async ({ readStream, filename, bucket, key }) => {
     readStream,
     filename
   });
-
   return await asyncUpload({
     key,
     contentType,
