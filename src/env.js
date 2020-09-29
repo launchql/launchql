81d0e0a264f411bfa3992073de2d29661ae4b2e2
Dan Lynch <pyramation@gmail.com>
2020-09-29 15:33:36 -0700

    first commit

---
+++ src/env.js
@@ -0,0 +1,14 @@
+import { cleanEnv, str, port } from 'envalid';
+
+export const env = cleanEnv(
+  process.env,
+  {
+    PGUSER: str({ default: 'postgres' }),
+    PGHOST: str({ default: 'localhost' }),
+    PGPASSWORD: str({ default: 'password' }),
+    PGPORT: port({ default: 5432 }),
+    PGDATABASE: str(),
+    SCHEMA: str()
+  },
+  { dotEnvPath: null }
+);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export * from './graphql';
+export * from './gql';
+export * from './env';
+export * from './clean';
