5e9f332dc51b169086ae4293c4ac77af4221dcdf
Dan Lynch <pyramation@gmail.com>
2020-10-14 00:10:17 -0700

    parse complex type

--- __tests__/csv2pg.test.js
+++ __tests__/csv2pg.test.js
@@ -1,31 +1,52 @@
 import { parse } from '../src';
 import { resolve } from 'path';
 import { deparse } from 'pgsql-parser';
-import { InsertStmt } from '../src/utils';
+import {
+  InsertStmt,
+  InsertStmtValues,
+  makePoly,
+  makePoint
+} from '../src/utils';
 
 const CSV = resolve(__dirname + '/../__fixtures__/zip-codes.csv');
 
-it('works', async () => {
+it('insert each one', async () => {
   const result = await parse(CSV);
-  if (!result.length) throw new Error('no fields!');
-  const fields = Object.keys(result[0]);
 
   const types = {
-    code: 'int',
+    zip: 'int',
     latitude: 'float',
     longitude: 'float',
-    bbox: 'text'
+    location: (record) => makePoint(record),
+    bbox: (record) => makePoly(record)
   };
 
   const nodes = result.map((record) => {
     return InsertStmt({
       schemaName: 'schema',
       tableName: 'table',
-      fields: fields.map((name) => ({ name })),
       types,
       record
     });
   });
-  // expect(nodes).toMatchSnapshot();
   expect(deparse(nodes)).toMatchSnapshot();
+});
+
+it('insert many', async () => {
+  const records = await parse(CSV);
+
+  const types = {
+    zip: 'int',
+    location: (record) => makePoint(record),
+    bbox: (record) => makePoly(record)
+  };
+
+  const stmt = InsertStmtValues({
+    schemaName: 'schema',
+    tableName: 'zipcodes',
+    types,
+    records
+  });
+
+  expect(deparse([stmt])).toMatchSnapshot();
 });
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,89 @@
 import * as ast from 'pg-ast';
 
-export const InsertStmt = ({
+const lstr = (bbox) => {
+  const [lng1, lat1, lng2, lat2] = bbox.split(',').map((a) => a.trim());
+  return `LINESTRING(${lng1} ${lat1}, ${lng1} ${lat2}, ${lng2} ${lat2}, ${lng2} ${lat1}, ${lng1} ${lat1})`;
+};
+
+const funcCall = (name, args) => {
+  return ast.FuncCall({
+    funcname: [ast.String({ str: name })],
+    args
+  });
+};
+
+const aflt = (num) => ast.A_Const({ val: ast.Float({ str: num }) });
+const aint = (num) => ast.A_Const({ val: ast.Integer({ ival: num }) });
+
+export const makePoint = ({ longitude, latitude }) => {
+  return funcCall('st_setsrid', [
+    funcCall('st_makepoint', [aflt(longitude), aflt(latitude)]),
+    aint(4326)
+  ]);
+};
+
+export const makePoly = ({ bbox }) => {
+  return funcCall('st_setsrid', [
+    funcCall('st_makepolygon', [
+      funcCall('st_geomfromtext', [
+        ast.A_Const({ val: ast.String({ str: lstr(bbox) }) })
+      ])
+    ]),
+    aint(4326)
+  ]);
+};
+
+const ValuesLists = ({ types, record }) =>
+  Object.entries(types).map(([field, type]) => {
+    if (typeof type === 'function') {
+      return type(record);
+    }
+    if (!record[field]) {
+      throw new Error('field/record mismatch');
+    }
+    switch (type) {
+      case 'text':
+        return ast.A_Const({ val: ast.String({ str: record[field] }) });
+      case 'int':
+        return ast.A_Const({ val: ast.Integer({ ival: record[field] }) });
+      case 'float':
+        return ast.A_Const({ val: ast.Float({ str: record[field] }) });
+      default:
+        return ast.A_Const({ val: ast.String({ str: record[field] }) });
+    }
+  });
+
+export const InsertStmt = ({ schemaName, tableName, types, record }) => ({
+  RawStmt: {
+    stmt: {
+      InsertStmt: {
+        relation: {
+          RangeVar: {
+            schemaname: schemaName,
+            relname: tableName,
+            inh: true,
+            relpersistence: 'p'
+          }
+        },
+        cols: Object.keys(types).map((field) => ast.ResTarget({ name: field })),
+        selectStmt: {
+          SelectStmt: {
+            valuesLists: [ValuesLists({ types, record })],
+            op: 0
+          }
+        },
+        override: 0
+      }
+    },
+    stmt_len: 1
+  }
+});
+
+export const InsertStmtValues = ({
   schemaName,
   tableName,
-  fields,
   types,
-  record
+  records
 }) => ({
   RawStmt: {
     stmt: {
@@ -18,24 +96,12 @@ export const InsertStmt = ({
             relpersistence: 'p'
           }
         },
-        cols: fields.map((field) => ast.ResTarget({ name: field.name })),
+        cols: Object.keys(types).map((field) => ast.ResTarget({ name: field })),
         selectStmt: {
           SelectStmt: {
-            valuesLists: [
-              Object.entries(record).map(([k, v]) => {
-                const type = types[k];
-                switch (type) {
-                  case 'text':
-                    return ast.A_Const({ val: ast.String({ str: v }) });
-                  case 'int':
-                    return ast.A_Const({ val: ast.Integer({ ival: v }) });
-                  case 'float':
-                    return ast.A_Const({ val: ast.Float({ str: v }) });
-                  default:
-                    return ast.A_Const({ val: ast.String({ str: v }) });
-                }
-              })
-            ],
+            valuesLists: records.map((record) =>
+              ValuesLists({ types, record })
+            ),
             op: 0
           }
         },
