0f0b90fbb9d7221dcfe0f68439b6ed48a715f9c5
Dan Lynch <pyramation@gmail.com>
2020-10-13 18:10:50 -0700

    updates

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,5 @@
 import * as ast from '../src';
+import { deparse } from 'pgsql-parser';
 
 it('works', () => {
   const obj = ast.A_Expr({
@@ -8,4 +9,5 @@ it('works', () => {
     rexpr: ast.Integer({ ival: 0 })
   });
   expect(obj).toMatchSnapshot();
+  expect(deparse([obj])).toMatchSnapshot();
 });
