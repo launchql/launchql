87effdd626998fac198cc3aba64168f766b235a1
Dan Lynch <pyramation@gmail.com>
2020-10-29 02:41:34 -0700

    pkg

--- packages/cli/src/lib/cmds/package.js
+++ packages/cli/src/lib/cmds/package.js
@@ -10,15 +10,15 @@ export default async (argv) => {
   const pkgPath = `${sqitchPath}/package.json`;
   const pkg = require(pkgPath);
 
-  const questions = [
-    {
-      name: 'version',
-      message: 'version',
-      default: pkg.version,
-      required: true
-    }
-  ];
+  // const questions = [
+  //   {
+  //     name: 'version',
+  //     message: 'version',
+  //     default: pkg.version,
+  //     required: true
+  //   }
+  // ];
 
-  const { version } = await prompt(questions, argv);
-  await writePackage(version, true);
+  // const { version } = await prompt(questions, argv);
+  await writePackage(pkg.version, true);
 };
