d1f2dab89c8f6cdcb81b0778eb8aba5a8c278e1f
Dan Lynch <pyramation@gmail.com>
2020-10-30 13:55:21 -0700

    update publish test

--- packages/db-utils/test/publish.test.js
+++ packages/db-utils/test/publish.test.js
@@ -1,16 +1,12 @@
-import { publish } from '../src/lib/publish';
-
+import { publish, publishAndBump } from '../src/lib/publish';
 import * as shell from 'shelljs';
 const TMPDIR = process.env.TMPDIR;
 import { random } from '../src/lib/random';
 import { resolve as pr } from 'path';
-import { sync as mkdirp } from 'mkdirp';
 import { sync as glob } from 'glob';
-import { sync as rimraf } from 'rimraf';
-import { writeFileSync } from 'fs';
 
 describe('publish', () => {
-  let dir, projDir;
+  let dir;
   beforeEach(async () => {
     dir = pr(TMPDIR + '/' + random());
     shell.cp('-r', __dirname + '/../__fixtures__/publish', dir);
@@ -25,6 +21,30 @@ describe('publish', () => {
     expect(filesBefore).toMatchSnapshot();
 
     await publish('secrets', 'minor');
+
+    const filesAfter = glob(dir + '/**')
+      .map((f) => f.replace(dir, '').replace(/^\//, ''))
+      .filter((i) => i);
+    expect(filesAfter).toMatchSnapshot();
+  });
+});
+
+describe('semver publish', () => {
+  let dir;
+  beforeEach(async () => {
+    dir = pr(TMPDIR + '/' + random());
+    shell.cp('-r', __dirname + '/../__fixtures__/publish', dir);
+    process.env.SKITCH_PATH = dir;
+    process.env.SQITCH_PATH = dir + '/packages/secrets';
+    process.chdir(dir);
+  });
+  it('publish an extension', async () => {
+    const filesBefore = glob(dir + '/**')
+      .map((f) => f.replace(dir, '').replace(/^\//, ''))
+      .filter((i) => i);
+    expect(filesBefore).toMatchSnapshot();
+
+    await publishAndBump('secrets', 'minor');
 
     const filesAfter = glob(dir + '/**')
       .map((f) => f.replace(dir, '').replace(/^\//, ''))
