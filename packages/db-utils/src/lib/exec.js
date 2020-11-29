cde8a847f1db362d1b51fac0d92fdba17b632e87
Dan Lynch <pyramation@gmail.com>
2020-11-28 18:08:35 -0700

    updates

--- packages/db-utils/src/lib/exec.js
+++ packages/db-utils/src/lib/exec.js
@@ -1,14 +1,14 @@
 import { exec as shellExec } from 'shelljs';
-import { PGUSER, PGPASSWORD, PGHOST, PGPORT, PATH } from '@launchql/db-env';
+import { PGUSER, PGPASSWORD, PGHOST, PGPORT } from '@launchql/db-env';
 
 export const execSync = (cmd, opts) => {
   shellExec(cmd, {
     env: {
+      ...process.env,
       PGUSER,
       PGPASSWORD,
       PGHOST,
-      PGPORT,
-      PATH
+      PGPORT
     },
     ...opts
   });
