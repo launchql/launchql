a080b70f82df42faaea9d0f875c1ecb87db4a867
Dan Lynch <pyramation@gmail.com>
2020-11-30 22:14:13 -0700

    new pkg

---
+++ packages/graphile-query/src/index.js
@@ -0,0 +1,74 @@
+import {
+  withPostGraphileContext,
+  createPostGraphileSchema
+} from '@pyramation/postgraphile';
+import { graphql } from 'graphql';
+import { print } from 'graphql/language/printer';
+
+export const getSchema = async (pool, settings) =>
+  await createPostGraphileSchema(pool, settings.schema, settings);
+
+export class GraphileQuery {
+  constructor({ schema, pool, settings }) {
+    if (!schema) throw new Error('requires a schema');
+    if (!pool) throw new Error('requires a pool');
+    if (!settings) throw new Error('requires graphile settings');
+
+    this.pool = pool;
+    this.schema = schema;
+    this.settings = settings;
+  }
+  // role is optional! it overrides anything from req passed in.
+  async query({ req = {}, query, variables, role }) {
+    const queryString = typeof query === 'string' ? query : print(query);
+    const { pgSettings: pgSettingsGenerator } = this.settings;
+    const pgSettings = role
+      ? { role }
+      : typeof pgSettingsGenerator === 'function'
+      ? await pgSettingsGenerator(req)
+      : pgSettingsGenerator;
+
+    return await withPostGraphileContext(
+      {
+        ...this.settings,
+        pgPool: this.pool,
+        pgSettings
+      },
+      async (context) => {
+        return await graphql(
+          this.schema,
+          queryString,
+          null,
+          { ...context },
+          variables
+        );
+      }
+    );
+  }
+}
+
+export class GraphileQuerySimple {
+  constructor({ schema, pool }) {
+    if (!schema) throw new Error('requires a schema');
+    if (!pool) throw new Error('requires a pool');
+    this.pool = pool;
+    this.schema = schema;
+  }
+  async query(query, variables) {
+    const queryString = typeof query === 'string' ? query : print(query);
+    return await withPostGraphileContext(
+      {
+        pgPool: this.pool
+      },
+      async (context) => {
+        return await graphql(
+          this.schema,
+          queryString,
+          null,
+          { ...context },
+          variables
+        );
+      }
+    );
+  }
+}
