56a98f81502917f57f58e6f752f9ad45af91483c
phatg <phatghaa@gmail.com>
2020-12-16 16:51:10 +0700

    fix: test config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   projects: ['<rootDir>/packages/*/jest.config.js'],
   coverageDirectory: '<rootDir>/coverage',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  }
 };
