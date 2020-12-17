ffbb5b0308075c32b2e5a398fbdaf12c2ea78b87
Dan Lynch <pyramation@gmail.com>
2020-12-16 22:00:41 -0700

    fixtures

---
+++ packages/graphile-gen/__tests__/api.test.js
@@ -0,0 +1,17 @@
+import { generate } from '../src';
+
+import { print } from 'graphql';
+import mutations from '../__fixtures__/api/mutations.json';
+import queries from '../__fixtures__/api/queries.json';
+
+it('generate', () => {
+  const gen = generate({ ...queries, ...mutations });
+
+  const output = Object.keys(gen).reduce((m, key) => {
+    if (gen[key]?.ast) {
+      m[key] = print(gen[key].ast);
+    }
+    return m;
+  }, {});
+  expect(output).toMatchSnapshot();
+});
