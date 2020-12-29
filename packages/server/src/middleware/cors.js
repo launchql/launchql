eb13e635968514c669bc00d6101c72f4d63a531c
Dan Lynch <pyramation@gmail.com>
2020-12-29 06:13:28 -0700

    cors

--- packages/server/src/middleware/cors.js
+++ packages/server/src/middleware/cors.js
@@ -8,15 +8,13 @@ export const cors = async (req, res, next) => {
     return [...mod.data.urls, ...m];
   }, []);
 
-  let corsOptions;
-  if (listOfDomains.indexOf(req.get('origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-
+  let corsOptions = { origin: false }; // disable CORS for this request
   const origin = req.get('origin');
   if (origin) {
+    if (listOfDomains.indexOf(req.get('origin')) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    }
+
     // TODO add only if in development
     const url = new URL(origin);
     const parts = url.hostname.split('.');
