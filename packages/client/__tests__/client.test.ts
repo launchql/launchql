24dc849b15f811c6e56aad2f9c7b1fdf03328cea
Dan Lynch <pyramation@gmail.com>
2025-01-10 14:13:30 -0800

    test

--- packages/client/__tests__/client.test.ts
+++ packages/client/__tests__/client.test.ts
@@ -1,17 +1,25 @@
 process.env.DATABASE_URL = 'postgres://postgres:password@localhost:5432/postgres';
+jest.setTimeout(30000); // Set timeout to 30 seconds for this test file
+
 import { PoolClient } from 'pg';
 
 import { Database } from '../src';
 
 const db = new Database();
 
-afterAll(async () => {
-  await db.shutdown();
+afterAll(() => {
+  db.shutdown();
 });
 
-it('getClient', async () => {
-  await db.withTransaction(async (client: PoolClient) => {
-    const result = await client.query('SELECT 1');
-    expect(result.rows.length).toBe(1);
+it('getClient', (done) => {
+  db.withTransaction(async (client: PoolClient) => {
+    try {
+      const result = await client.query('SELECT 1');
+      expect(result.rows.length).toBe(1);
+      done();
+    } catch (error) {
+      console.error('Error executing query:', error);
+      done(error); 
+    }
   });
 });
