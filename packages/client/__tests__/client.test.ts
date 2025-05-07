947ccf281a8799d4e814eb06a3f7a6448a5a3eb3
Dan Lynch <pyramation@gmail.com>
2025-05-07 00:23:40 -0700

    test

--- packages/client/__tests__/client.test.ts
+++ packages/client/__tests__/client.test.ts
@@ -1,26 +1,20 @@
 process.env.DATABASE_URL = 'postgres://postgres:password@localhost:5432/postgres';
 import { PoolClient } from 'pg';
-
 import { Database } from '../src';
+
 let client: Database;
 
 beforeAll(() => {
   client = new Database();
-})
-
-afterAll(() => {
-  client.shutdown();
 });
 
-it('getClient', (done) => {
-  client.withTransaction(async (client: PoolClient) => {
-    try {
-      const result = await client.query('SELECT 1');
-      console.log(result.rows);
-      done();
-    } catch (error) {
-      console.error('Error executing query:', error);
-      done(error); 
-    }
+afterAll(async () => {
+  await client.shutdown();
+});
+
+it('getClient', async () => {
+  await client.withTransaction(async (client: PoolClient) => {
+    const result = await client.query('SELECT 1');
+    expect(result.rows[0]['?column?'] || result.rows[0].count).toBe(1);
   });
 });
