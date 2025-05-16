29d4c353f7ab71fbd391ede3e4dc36c4a5cf0e24
Dan Lynch <pyramation@gmail.com>
2025-05-16 16:46:11 -0400

    setup

--- testing/my-third/__tests__/first.test.ts
+++ testing/my-third/__tests__/first.test.ts
@@ -4,4 +4,7 @@ import { resolve } from 'path';
 it('LaunchQL', () => {
     const project = new LaunchQLProject(resolve(__dirname+'/../'));
     console.log(project);
+
+    const plan = project.getModulePlan();
+    console.log(plan);
 });
