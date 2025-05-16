ab6c4d0a6d8c21048c759ddc0e9a3eb402d22223
Dan Lynch <pyramation@gmail.com>
2025-05-16 16:53:24 -0400

    createdb in test

--- testing/my-third/__tests__/first.test.ts
+++ testing/my-third/__tests__/first.test.ts
@@ -1,10 +1,21 @@
-import { LaunchQLProject } from '@launchql/migrate';
+import { deploy, LaunchQLProject } from '@launchql/migrate';
 import { resolve } from 'path';
+import { LaunchQLOptions, getEnvOptions } from '@launchql/types';
+import { randomInt } from 'crypto';
+import { execSync } from 'child_process';
 
-it('LaunchQL', () => {
+it('LaunchQL', async () => {
     const project = new LaunchQLProject(resolve(__dirname+'/../'));
     console.log(project);
 
     const plan = project.getModulePlan();
     console.log(plan);
+
+    const opts = getEnvOptions({
+        pg: {
+            database: 'db-'+randomInt(1000)
+        }
+    })
+    execSync(`createdb ${opts.pg.database}`);
+    await deploy(opts, 'my-third', opts.pg.database, project.modulePath);
 });
