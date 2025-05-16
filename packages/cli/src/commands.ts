cdafeb9f435bd790d8ce43ad21379d8cea1f1cc1
Dan Lynch <pyramation@gmail.com>
2025-05-16 16:05:13 -0400

    cmds

--- packages/cli/src/commands.ts
+++ packages/cli/src/commands.ts
@@ -4,6 +4,7 @@ import { ParsedArgs } from 'minimist';
 import { readAndParsePackageJson } from './package';
 import { extractFirst, usageText } from './utils';
 
+// Commands
 import deploy from './commands/deploy';
 import server from './commands/server';
 import explorer from './commands/explorer';
@@ -16,71 +17,70 @@ import _export from './commands/export';
 import _package from './commands/package';
 import kill from './commands/kill';
 
+// Command map
+const commandMap: Record<string, Function> = {
+  deploy,
+  server,
+  explorer,
+  verify,
+  revert,
+  init,
+  extension,
+  plan,
+  export: _export,
+  package: _package,
+  kill
+};
+
 export const commands = async (argv: Partial<ParsedArgs>, prompter: Inquirerer, options: CLIOptions) => {
-    if (argv.version || argv.v) {
-        const pkg = readAndParsePackageJson()
-        console.log(pkg.version);
-        process.exit(0);
-    }
+  if (argv.version || argv.v) {
+    const pkg = readAndParsePackageJson();
+    console.log(pkg.version);
+    process.exit(0);
+  }
 
-    let { first: command, newArgv } = extractFirst(argv);
+  let { first: command, newArgv } = extractFirst(argv);
 
-    if (argv.help || argv.h || command === 'help' || !command) {
-        console.log(usageText);
-        process.exit(0);
-    }
+  // Show usage if explicitly requested
+  if (argv.help || argv.h || command === 'help') {
+    console.log(usageText);
+    process.exit(0);
+  }
 
-    newArgv = await prompter.prompt(newArgv, [
-        {
-            type: 'text',
-            name: 'cwd',
-            message: 'Working directory',
-            required: false,
-            default: process.cwd(),
-            useDefault: true
-        }
+  // Prompt if no command provided
+  if (!command) {
+    const answer = await prompter.prompt(argv, [
+      {
+        type: 'autocomplete',
+        name: 'command',
+        message: 'What do you want to do?',
+        options: Object.keys(commandMap)
+      }
     ]);
+    command = answer.command;
+  }
 
-    switch (command) {
-        case 'deploy':
-            await deploy(newArgv, prompter, options);
-            break;
-        case 'verify':
-            await verify(newArgv, prompter, options);
-            break;
-        case 'revert':
-            await revert(newArgv, prompter, options);
-            break;
-        case 'init':
-            await init(newArgv, prompter, options);
-            break;
-        case 'server':
-            await server(newArgv, prompter, options);
-            break;
-        case 'explorer':
-            await explorer(newArgv, prompter, options);
-            break;
-        case 'extension':
-            await extension(newArgv, prompter, options);
-            break;
-        case 'plan':
-            await plan(newArgv, prompter, options);
-            break;
-        case 'package':
-            await _package(newArgv, prompter, options);
-            break;
-        case 'export':
-            await _export(newArgv, prompter, options);
-            break;
-        case 'kill':
-            await kill(newArgv, prompter, options);
-            break;
-        default:
-            console.error(`Unknown command: ${command}`);
-            console.log(usageText);
-            process.exit(1);
+  // Prompt for working directory
+  newArgv = await prompter.prompt(newArgv, [
+    {
+      type: 'text',
+      name: 'cwd',
+      message: 'Working directory',
+      required: false,
+      default: process.cwd(),
+      useDefault: true
     }
+  ]);
 
-    prompter.close();
-    return argv;
+  const commandFn = commandMap[command];
+
+  if (!commandFn) {
+    console.error(`Unknown command: ${command}`);
+    console.log(usageText);
+    process.exit(1);
+  }
+
+  await commandFn(newArgv, prompter, options);
+  prompter.close();
+  return argv;
 };
