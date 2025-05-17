d9a8145abb5a8931dde41bb03072d0ae4ede36ae
Dan Lynch <pyramation@gmail.com>
2025-05-17 14:51:54 -0700

    clean pkgs

--- packages/server-utils/src/env.ts
+++
@@ -1,8 +0,0 @@
-import { cleanEnv, str, port } from 'envalid';
-
-export const env = cleanEnv(process.env, {
-    PGUSER: str({ default: 'postgres' }),
-    PGHOST: str({ default: 'localhost' }),
-    PGPASSWORD: str({ default: 'password' }),
-    PGPORT: port({ default: 5432 })
-});
