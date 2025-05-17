f98a975707ea8e3ba7dc441fd18a6f0af28f0790
Dan Lynch <pyramation@gmail.com>
2025-05-16 21:10:09 -0400

    JS -> TS

--- packages/graphile-test/src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './graphql';
-export * from './gql';
-export * from './env';
-export * from './clean';
--- packages/graphile-test/utils/env.js
+++
@@ -1,2 +0,0 @@
-process.env.SCHEMA = 'app_public';
-process.env.PGDATABASE = 'test_database';
