32c6928aba084653bd211a2dd7d559b1940168a9
Dan Lynch <pyramation@gmail.com>
2025-05-18 03:13:39 -0700

    PR: 19

--- packages/launchql-gen/__tests__/granular.test.ts
+++ packages/launchql-gen/__tests__/granular.test.ts
@@ -3,6 +3,8 @@ import { generateGranular as generate } from '../src';
 import { print } from 'graphql';
 import mutations from '../__fixtures__/api/mutations.json';
 import queries from '../__fixtures__/api/queries.json';
+// import queryNestedSelectionMany from '../__fixtures__/api/query-nested-selection-many.json';
+// import queryNestedSelectionOne from '../__fixtures__/api/query-nested-selection-one.json';
 
 it('generate', () => {
   // @ts-ignore
@@ -11,7 +13,31 @@ it('generate', () => {
     'name',
     'approved'
   ]);
+  
+  const output = Object.keys(gen).reduce((m, key) => {
+    if (gen[key]?.ast) {
+      // @ts-ignore
+      m[key] = print(gen[key].ast);
+    }
+    return m;
+  }, {});
+  expect(output).toMatchSnapshot();
+});
 
+it('generate w count info', () => {
+  const gen = generate(
+    { ...queries, ...mutations },
+    'ActionItem',
+    ['userActionItems'],
+    // @ts-ignore
+    {
+      userActionItems: {
+        first: 10,
+        offset: 10
+      }
+    }
+  );
+  
   const output = Object.keys(gen).reduce((m, key) => {
     if (gen[key]?.ast) {
       // @ts-ignore
