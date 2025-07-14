fec546217a0ed04cba9bdaf08e385c5101054345
Dan Lynch <pyramation@gmail.com>
2025-07-13 22:46:57 -0700

    don't re-export

--- packages/migrate/src/index.ts
+++ packages/migrate/src/index.ts
@@ -3,6 +3,3 @@ export * from './types';
 export { hashFile, hashString } from './utils/hash';
 export { readScript, scriptExists } from './utils/fs';
 export { withTransaction, TransactionContext, TransactionOptions } from './utils/transaction';
-
-// Re-export parser functionality from sqitch-parser
-export * from '@launchql/sqitch-parser';
