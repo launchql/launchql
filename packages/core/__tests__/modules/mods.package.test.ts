7192ffc8ce90c286f822b46a09ccd94a76bef316
Dan Lynch <pyramation@gmail.com>
2025-07-15 22:20:03 -0700

    snaps

--- packages/core/__tests__/modules/mods.package.test.ts
+++ packages/core/__tests__/modules/mods.package.test.ts
@@ -14,11 +14,11 @@ afterAll(() => {
 describe('packageModule tests', () => {
   it('creates an extension', async () => {
     const { sql } = await packageModule(fixture.tempFixtureDir);
-    expect(cleanText(sql)).toMatchSnapshot();
+    expect(sql).toMatchSnapshot();
   });
 
   it('creates an extension via plan', async () => {
     const { sql } = await packageModule(fixture.tempFixtureDir, { usePlan: true });
-    expect(cleanText(sql)).toMatchSnapshot();
+    expect(sql).toMatchSnapshot();
   });
 });
