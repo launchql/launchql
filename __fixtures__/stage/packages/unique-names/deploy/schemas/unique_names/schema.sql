d54f732343ec5b8791c8534a29ee37e15cd4c8e2
Dan Lynch <pyramation@gmail.com>
2025-08-07 23:35:51 +0000

    test(fixture): remove temporary requires line now that plan-only resolution is used
    Co-Authored-By: Dan Lynch <pyramation@gmail.com>

--- __fixtures__/stage/packages/unique-names/deploy/schemas/unique_names/schema.sql
+++ __fixtures__/stage/packages/unique-names/deploy/schemas/unique_names/schema.sql
@@ -1,8 +1,5 @@
 -- Deploy schemas/unique_names/schema to pg
 
--- TODO remove this once we can use control file deps
--- requires: launchql-ext-default-roles:@0.0.5
-
 BEGIN;
 
 CREATE SCHEMA unique_names;
