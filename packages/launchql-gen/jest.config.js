45fa8cefabd7946a9431939bda9a6a300a6322cf
Eason <sdqede@gmail.com>
2025-11-03 19:23:34 +1300

    fix(launchql-gen): add Jest moduleNameMapper for workspace dependencies
    - Add moduleNameMapper configuration in packages/launchql-gen/jest.config.js
    - Maps @launchql/* packages to packages/*/dist directories
    - Maps pgsql-test, graphile-test, pg-env, and pg-cache to their dist directories
    - Fixes Jest module resolution issues with pnpm's isolated structure
    - Required for tests that import graphile-test which depends on pgsql-test

--- packages/launchql-gen/jest.config.js
+++ packages/launchql-gen/jest.config.js
@@ -14,5 +14,13 @@ module.exports = {
   transformIgnorePatterns: [`/node_modules/*`],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePathIgnorePatterns: ['dist/*']
+  modulePathIgnorePatterns: ['dist/*'],
+  moduleNameMapper: {
+    '^@launchql/logger$': '<rootDir>/../../packages/logger/dist',
+    '^@launchql/(.*)$': '<rootDir>/../../packages/$1/dist',
+    '^pgsql-test$': '<rootDir>/../../packages/pgsql-test/dist',
+    '^graphile-test$': '<rootDir>/../../packages/graphile-test/dist',
+    '^pg-env$': '<rootDir>/../../packages/pg-env/dist',
+    '^pg-cache$': '<rootDir>/../../packages/pg-cache/dist',
+  },
 };
