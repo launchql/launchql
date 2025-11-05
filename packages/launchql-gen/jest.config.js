c56a849522c6502b4ba329609aaf01509d5a4c54
Eason <sdqede@gmail.com>
2025-11-05 17:32:37 +1300

    refactor: migrate remaining packages to workspace:^ protocol and remove Jest moduleNameMapper
    - Replace workspace dependencies with workspace:^ protocol in:
      * packages/pgsql-test (dependencies)
      * packages/launchql-gen (devDependencies)
      * packages/pg-codegen (dependencies)
      * packages/introspectron (devDependencies)
    
    - Remove moduleNameMapper from Jest configs:
      * packages/pgsql-test/jest.config.js
      * packages/launchql-gen/jest.config.js
      * packages/pg-codegen/jest.config.js
      * packages/introspectron/jest.config.js
    
    This completes the migration to pnpm's native workspace protocol,
    allowing Jest to resolve workspace packages via pnpm's symlink structure
    without manual module path mappings.

--- packages/launchql-gen/jest.config.js
+++ packages/launchql-gen/jest.config.js
@@ -15,12 +15,4 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['dist/*'],
-  moduleNameMapper: {
-    '^@launchql/logger$': '<rootDir>/../../packages/logger/dist',
-    '^@launchql/(.*)$': '<rootDir>/../../packages/$1/dist',
-    '^pgsql-test$': '<rootDir>/../../packages/pgsql-test/dist',
-    '^graphile-test$': '<rootDir>/../../packages/graphile-test/dist',
-    '^pg-env$': '<rootDir>/../../packages/pg-env/dist',
-    '^pg-cache$': '<rootDir>/../../packages/pg-cache/dist',
-  },
 };
