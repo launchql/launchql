5b505dc2e2e3e6a181d9c3f942a5dc86eec6d39d
Dan Lynch <pyramation@gmail.com>
2025-11-13 14:17:42 -0800

    guard role creation with catching exceptions for concurrent tests

--- packages/core/src/init/sql/bootstrap-roles.sql
+++ packages/core/src/init/sql/bootstrap-roles.sql
@@ -1,45 +1,55 @@
--- anonymous
-CREATE ROLE anonymous;
+BEGIN;
+DO $do$
+BEGIN
+  -- anonymous
+  BEGIN
+    EXECUTE format('CREATE ROLE %I', 'anonymous');
+  EXCEPTION
+    WHEN duplicate_object THEN
+      -- Role already exists; optionally sync attributes here with ALTER ROLE
+      NULL;
+  END;
+  
+  -- authenticated
+  BEGIN
+    EXECUTE format('CREATE ROLE %I', 'authenticated');
+  EXCEPTION
+    WHEN duplicate_object THEN
+      -- Role already exists; optionally sync attributes here with ALTER ROLE
+      NULL;
+  END;
+  
+  -- administrator
+  BEGIN
+    EXECUTE format('CREATE ROLE %I', 'administrator');
+  EXCEPTION
+    WHEN duplicate_object THEN
+      -- Role already exists; optionally sync attributes here with ALTER ROLE
+      NULL;
+  END;
+END
+$do$;
 
+-- Set role attributes (safe to run even if role already exists)
 ALTER USER anonymous WITH NOCREATEDB;
-
 ALTER USER anonymous WITH NOSUPERUSER;
-
 ALTER USER anonymous WITH NOCREATEROLE;
-
 ALTER USER anonymous WITH NOLOGIN;
-
 ALTER USER anonymous WITH NOREPLICATION;
-
 ALTER USER anonymous WITH NOBYPASSRLS;
 
--- authenticated
-CREATE ROLE authenticated;
-
 ALTER USER authenticated WITH NOCREATEDB;
-
 ALTER USER authenticated WITH NOSUPERUSER;
-
 ALTER USER authenticated WITH NOCREATEROLE;
-
 ALTER USER authenticated WITH NOLOGIN;
-
 ALTER USER authenticated WITH NOREPLICATION;
-
 ALTER USER authenticated WITH NOBYPASSRLS;
 
--- administrator
-CREATE ROLE administrator;
-
 ALTER USER administrator WITH NOCREATEDB;
-
 ALTER USER administrator WITH NOSUPERUSER;
-
 ALTER USER administrator WITH NOCREATEROLE;
-
 ALTER USER administrator WITH NOLOGIN;
-
 ALTER USER administrator WITH NOREPLICATION;
-
 -- they CAN bypass RLS
 ALTER USER administrator WITH BYPASSRLS;
+COMMIT;
