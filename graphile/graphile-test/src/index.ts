305adff167b010dfdbb089c00f6485a2712e43b6
Lucas <2862605953@qq.com>
2025-12-03 11:52:53 +0800

    feat(graphile-test): export bootstrapRoles from package
    Export bootstrapRoles() function so it can be imported by consumers
    of the graphile-test package.

--- graphile/graphile-test/src/index.ts
+++ graphile/graphile-test/src/index.ts
@@ -2,4 +2,5 @@ export * from './context';
 export * from './get-connections';
 export * from './graphile-test';
 export * from './types';
+export * from './seed-roles';
 export { seed, snapshot } from 'pgsql-test';
