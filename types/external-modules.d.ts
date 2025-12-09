3a5cc231e7e042aca03e5086fcd88195a6b3fcad
Anmol1696 <anmol1696@gmail.com>
2025-12-09 23:36:30 +0530

    get a working module for functions, add a full setup

--- types/external-modules.d.ts
+++ types/external-modules.d.ts
@@ -8,6 +8,8 @@ declare module 'node-schedule';
 declare module 'request';
 declare module 'express';
 declare module 'body-parser';
+declare module '@launchql/mjml';
+declare module '@launchql/postmaster';
 
 declare module '@launchql/job-pg';
 declare module '@launchql/job-utils';
